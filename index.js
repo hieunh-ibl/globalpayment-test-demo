612d62e38c6c2f086a8cbb23730c310829a5494d
nhhieu-ibl <hieunh@blockchainlabs.asia>
2019-02-25 11:54:26 +0700

    refactor code

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Payment = require('globalpayments-api');
 const formData = require('urlencoded-body-parser');
-const { Address,
-  BuilderError,
+
+const { parseBase64String } = require('./utils');
+const {
   HostedPaymentConfig,
   HostedService,
   ServicesConfig,
@@ -38,26 +39,13 @@ module.exports = async (req, res) => {
   switch (req.method) {
     case 'GET':
       if (req.url.indexOf('/get-json') === 0) {
-        let json = service
+        const payData = service
           .charge(1)
           .withCurrency("USD")
           .serialize();
-    
-          json = JSON.parse(json);
-    
-          json.MERCHANT_ID = config.merchantId;
-          json.ACCOUNT = config.accountId;
-          json.AMOUNT = Buffer.from(json.AMOUNT, 'base64').toString();
-          json.CURRENCY = Buffer.from(json.CURRENCY, 'base64').toString();
-          json.TIMESTAMP = Buffer.from(json.TIMESTAMP, 'base64').toString();
-          json.AUTO_SETTLE_FLAG = Buffer.from(json.AUTO_SETTLE_FLAG, 'base64').toString();
-          json.MERCHANT_RESPONSE_URL = Buffer.from(json.MERCHANT_RESPONSE_URL, 'base64').toString();
-          json.HPP_LANG = Buffer.from(json.HPP_LANG, 'base64').toString();
-          json.SHA1HASH = Buffer.from(json.SHA1HASH, 'base64').toString();
-          json.ORDER_ID = Buffer.from(json.ORDER_ID, 'base64').toString();
-    
-          res.end(JSON.stringify(json));
-          return;
+        
+        res.end(parseBase64String(payData));
+        return;
       }
       if (req.url.indexOf('/rxp-js.js') === 0) {
         await showRxp(res);
@@ -67,7 +55,7 @@ module.exports = async (req, res) => {
       break;
     case 'POST':
       const data = await formData(req);
-      console.log("data ++++++", data);
+      console.log("data ++++++", parseBase64String(data));
       await showIndex(res);
       break;
   }
